f6160c9fb4d0ea8eeb4e1cdc64741ce517865d9d
donnie gil <gildonnie864@gmail.com>
2024-01-05 13:56:38 -0800

    set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 // import { useState } from 'react'
 import './App.css'
-import { Button } from 'react-bootstrap';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Login from './Pages/Login';
+import Chat from './Pages/Chat-area';
+
 
 function App() {
-
+  const location = useLocation();
   return (
-    <div>
-        <h1>This is the start of the group project</h1>
-        <Button variant="primary">Primary</Button>{' '}
-    </div>
+    <Routes key={location.pathname} location={location} >
+      <Route index element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
   )
 }
 
