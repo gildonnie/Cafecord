6d5d81b2f9c7e29182c6db038f3fc66a32f6ee0f
Alexandro Aguilera <navysealalex@gmail.com>
2024-02-07 16:00:01 -0800

    Updated some styling on the side menu in mobile view and some styling on the products page

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -5,28 +5,36 @@ function Products() {
 
     return (
         <div className="prodBackground">
-            <div className="productsTitle"><h1>Products</h1>
-                <h2><Link to={'/Chat'}>Back to Chat</Link></h2>
+            <div className="productsTitle">
+                <h1>Products</h1>
             </div>
             <div className="products">
 
                 <img src="/assets/Cafecord-product.png"></img>
                 <h2>Up Coffee</h2>
                 <p>Made from a small batch roaster, Up coffee will be sure to put a pep to your step!</p>
+                <p>$25 USD</p>
 
                 <img src="/assets/Cafecord-product2.png"></img>
                 <h2>COFFEE</h2>
                 <p>From the makers of Up coffee, its COFFEE coffee, with subtle notes of coffee, COFFEE will satisfy any coffee lover!</p>
+                <p>$25 USD</p>
 
                 <img src="/assets/Cafecord-product3.png"></img>
                 <h2>Limited Edition: Azul Seca coffee</h2>
                 <p>Hidden high in the Azul mountains this coffee is grown, and roasted to perfection! The taste alone will transport you to the peak of coffee eurphoria!</p>
+                <p>$30 USD</p>
 
                 <img src="/assets/Cafecord-product4.png"></img>
                 <h2>COFFEE To Go</h2>
                 <p>The masses asked for it and we delivered! COFFEE to go packets are here! Now you can have the incredibly satisfying COFFEE wherever you are!</p>
+                <p>$40 USD pack of 24</p>
 
             </div>
+            <div className="productsLinkDecor">
+                <h2><Link to={'/Chat'} className="productsLink">Back to Chat</Link></h2>
+            </div>
+
 
 
         </div>
